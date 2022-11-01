4777e4d6968092a95495fb8759164a21d6802d03
Alexey <antonov1972@yandex.ru>
2022-11-01 15:39:25 +0300

    Print result

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -5,18 +5,18 @@ string[]array = new string[n];
 string[]array3 = new string[n];
 
 void DataEnter(string[]_array)
-{for (int i = 0; i < n; i++)
 {
-    Console.Write($"Введите {i} элемент строкового массива: ");
-    array[i] = Console.ReadLine();
-    
-}
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Введите {i} элемент строкового массива: ");
+        array[i] = Console.ReadLine();
+    }
 }
  void PrintArray(string[]_array)
 {
     for (int i = 0; i < n; i++)
     {
-        Console.Write($"{_array[i]}, ");
+       Console.Write($"{_array[i]}, ");
     }
     Console.WriteLine();
 }
@@ -25,16 +25,23 @@ void CreatNewArray(string[]_array, string[]_array3)
     for (int i = 0; i < n; i++)
     {
         if (_array[i].Length <= str)
-        _array3[i] = _array[i];
-        Console.Write($"{array[i].Length}, ");
+            _array3[i] = _array[i];
     }
     Console.WriteLine();
 }
-
+void PrintArray3(string[]_array3)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (_array3[i] != null)
+        Console.Write($"{_array3[i]}, ");
+    }
+    Console.WriteLine();
+}
  DataEnter(array);
  PrintArray(array);
  CreatNewArray(array, array3);
- PrintArray(array3);
+ PrintArray3(array3);
  
  
  
