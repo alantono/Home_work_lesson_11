022bc4190a499e41b0ae839204f242012c23ae6e
Alexey <antonov1972@yandex.ru>
2022-11-01 13:57:09 +0300

    FillArray part

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Программа, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символам.
+int n = 5;
+string[]array = new string[n];
+
+for (int i = 0; i < n - 1; i++)
+{
+    Console.Write($"Введите {i} элемент строкового массива: ");
+    string.TryParse(Console.ReadLine()!, out array[i]);
+}
+
+
 Console.WriteLine("Hello, World!");
